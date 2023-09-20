e04fe5f9e7eb5ee1ceb7cf53bd4c3aea8b4f0898
neetmehta <neetmehta19@gmail.com>
2023-09-19 23:03:47 -0500

    lidar bounding boxes

---
+++ bounding_box.py
@@ -0,0 +1,131 @@
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+
+# ==============================================================================
+# -- imports -------------------------------------------------------------------
+# ==============================================================================
+
+import carla
+
+import weakref
+import random
+import numpy as np
+
+class ClientSideBoundingBoxes(object):
+    """
+    This is a module responsible for creating 3D bounding boxes and drawing them
+    client-side on pygame surface.
+    """
+
+    @staticmethod
+    def get_bounding_boxes(ego_vehicle, vehicles, sensor):
+        """
+        Creates 3D bounding boxes based on carla vehicle list and sensor.
+        """
+
+        bounding_boxes = [ClientSideBoundingBoxes.get_bounding_box(vehicle, sensor) for vehicle in vehicles if vehicle.id != ego_vehicle.id]
+
+        return bounding_boxes
+    
+    @staticmethod
+    def get_bounding_box(vehicle, sensor):
+        """
+        Returns 3D bounding box for a vehicle based on sensor view.
+        """
+
+        bb_cords = ClientSideBoundingBoxes._create_bb_points(vehicle)
+        cords_x_y_z = ClientSideBoundingBoxes._vehicle_to_sensor(bb_cords, vehicle, sensor)[:3, :]
+
+        return cords_x_y_z
+    
+    @staticmethod
+    def _create_bb_points(vehicle):
+        """
+        Returns 3D bounding box for a vehicle.
+        """
+
+        cords = np.zeros((8, 4))
+        extent = vehicle.bounding_box.extent
+        cords[0, :] = np.array([extent.x, extent.y, -extent.z, 1])
+        cords[1, :] = np.array([-extent.x, extent.y, -extent.z, 1])
+        cords[2, :] = np.array([-extent.x, -extent.y, -extent.z, 1])
+        cords[3, :] = np.array([extent.x, -extent.y, -extent.z, 1])
+        cords[4, :] = np.array([extent.x, extent.y, extent.z, 1])
+        cords[5, :] = np.array([-extent.x, extent.y, extent.z, 1])
+        cords[6, :] = np.array([-extent.x, -extent.y, extent.z, 1])
+        cords[7, :] = np.array([extent.x, -extent.y, extent.z, 1])
+        return cords
+    
+    @staticmethod
+    def _vehicle_to_sensor(cords, vehicle, sensor):
+        """
+        Transforms coordinates of a vehicle bounding box to sensor.
+        """
+
+        world_cord = ClientSideBoundingBoxes._vehicle_to_world(cords, vehicle)
+        sensor_cord = ClientSideBoundingBoxes._world_to_sensor(world_cord, sensor)
+        return sensor_cord
+
+    @staticmethod
+    def _vehicle_to_world(cords, vehicle):
+        """
+        Transforms coordinates of a vehicle bounding box to world.
+        """
+
+        bb_transform = carla.Transform(vehicle.bounding_box.location)
+        bb_vehicle_matrix = ClientSideBoundingBoxes.get_matrix(bb_transform)
+        vehicle_world_matrix = ClientSideBoundingBoxes.get_matrix(vehicle.get_transform())
+        bb_world_matrix = np.dot(vehicle_world_matrix, bb_vehicle_matrix)
+        world_cords = np.dot(bb_world_matrix, np.transpose(cords))
+        return world_cords
+
+    @staticmethod
+    def _world_to_sensor(cords, sensor):
+        """
+        Transforms world coordinates to sensor.
+        """
+
+        sensor_world_matrix = ClientSideBoundingBoxes.get_matrix(sensor.get_transform())
+        world_sensor_matrix = np.linalg.inv(sensor_world_matrix)
+        sensor_cords = np.dot(world_sensor_matrix, cords)
+        return sensor_cords
+    
+    @staticmethod
+    def get_matrix(transform):
+        """
+        Creates matrix from carla transform.
+        """
+
+        rotation = transform.rotation
+        location = transform.location
+        c_y = np.cos(np.radians(rotation.yaw))
+        s_y = np.sin(np.radians(rotation.yaw))
+        c_r = np.cos(np.radians(rotation.roll))
+        s_r = np.sin(np.radians(rotation.roll))
+        c_p = np.cos(np.radians(rotation.pitch))
+        s_p = np.sin(np.radians(rotation.pitch))
+        matrix = np.matrix(np.identity(4))
+        matrix[0, 3] = location.x
+        matrix[1, 3] = location.y
+        matrix[2, 3] = location.z
+        matrix[0, 0] = c_p * c_y
+        matrix[0, 1] = c_y * s_p * s_r - s_y * c_r
+        matrix[0, 2] = -c_y * s_p * c_r - s_y * s_r
+        matrix[1, 0] = s_y * c_p
+        matrix[1, 1] = s_y * s_p * s_r + c_y * c_r
+        matrix[1, 2] = -s_y * s_p * c_r + c_y * s_r
+        matrix[2, 0] = s_p
+        matrix[2, 1] = -c_p * s_r
+        matrix[2, 2] = c_p * c_r
+        return matrix
--- main.py
+++ main.py
@@ -3,18 +3,38 @@ from ego_vehicle import EgoVehicle
 import yaml
 import cv2
 import numpy as np
+import glob
+import os
+import sys
+import matplotlib.pyplot as plt
+import open3d as o3d
+import time
 
-from utils import process_rgb_image, retrive_data, process_sem_seg_image, process_depth_image
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+import carla
+
+from utils import process_rgb_image, retrive_data, process_depth_image, process_sem_seg_image, process_point_cloud, add_open3d_axis
+from bounding_box import ClientSideBoundingBoxes
+
+lines = [[0, 1], [1, 2], [2, 3], [3, 0],
+         [4, 5], [5, 6], [6, 7], [7, 4],
+         [0, 4], [1, 5], [2, 6], [3, 7]]
 
 def main():
     with open('cfg\\vehicle_cfg.yaml', 'r') as f:
         vehicle_cfg = yaml.safe_load(f) 
-        
+    
     with open('cfg\\config.yaml', 'r') as f:
         cfg = yaml.safe_load(f) 
         
     carla_world = CarlaWorld(cfg)
-    carla_world.spawn_actors(10)
+    carla_world.spawn_actors()
     bp_lib = carla_world.world.get_blueprint_library()
     ego_vehicle = EgoVehicle(bp_lib, vehicle_cfg)
     ego_vehicle.spwan_ego_vehicle(carla_world.world)
@@ -23,8 +43,21 @@ def main():
     for sensor_name, sensor in ego_vehicle.sensors.items():
         sensor.listen(ego_vehicle.sensors_queues[sensor_name].put)
     carla_world.set_synchronous()
+    point_list = o3d.geometry.PointCloud()
 
-    i=0
+    # Open3D visualiser for LIDAR and RADAR
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(
+        window_name='Carla Lidar',
+        width=960,
+        height=540,
+        left=480,
+        top=270)
+    vis.get_render_option().background_color = [1.0, 1.0, 1.0]
+    vis.get_render_option().point_size = 1
+    vis.get_render_option().show_coordinate_frame = True
+    add_open3d_axis(vis)
+    frame = 0
     while True:
         data = {}
         frame_id = carla_world.tick()
@@ -32,19 +65,54 @@ def main():
             data[sensor_name] = retrive_data(ego_vehicle.sensors_queues[sensor_name], frame_id, 2.0)
         
         rgb_array = process_rgb_image(data['rgb_camera1'])
-        sem_seg_array = process_sem_seg_image(data['sem_seg_camera1'])
-        depth_array = process_depth_image(data['depth_camera1'])
+        # sem_seg_array = process_sem_seg_image(data['sem_seg_camera1'])
+        # depth_array = process_depth_image(data['depth_camera1'])
+        process_point_cloud(data['lidar1'], point_list)
+        vehicles = carla_world.world.get_actors().filter('vehicle.*')
+        bounding_boxes = ClientSideBoundingBoxes.get_bounding_boxes(ego_vehicle.ego_vehicle, vehicles, ego_vehicle.sensors['lidar1'])
+        for box in bounding_boxes:
+            box[0,:] *= -1
+
+        if frame == 2:
+            vis.add_geometry(point_list)
+            line_sets = []
+            for box in bounding_boxes:
+                line_set = o3d.geometry.LineSet()
+                line_set.points = o3d.utility.Vector3dVector(np.asarray(box.T))
+                line_set.lines = o3d.utility.Vector2iVector(lines)
+                vis.add_geometry(line_set)
+                line_sets.append(line_set)
+            
+        for i, box in enumerate(bounding_boxes):
+            line_sets[i].points = o3d.utility.Vector3dVector(np.asarray(box.T))
+            vis.update_geometry(line_sets[i])
+            
+        vis.update_geometry(point_list)
+
+        for line_set in line_sets:
+            vis.update_geometry(line_set)
+        
+        vis.poll_events()
+        vis.update_renderer()
+        
+        # # This can fix Open3D jittering issues:
+        time.sleep(0.005)
+        frame += 1
         cv2.imshow('RGB Camera', rgb_array)
-        cv2.imshow('Depth Camera', depth_array)
-        cv2.imshow('Semantic Camera', sem_seg_array)
+        # cv2.imshow('Depth Camera', depth_array)
+        # cv2.imshow('Semantic Camera', sem_seg_array)
 
         # Quit if user presses 'q'
         if cv2.waitKey(1) == ord('q'):
             cv2.destroyAllWindows()
+            vis.destroy_window()
             
             break
+        
+
     carla_world.restore()
     carla_world.destroy_actors()
+
     
 if __name__ == "__main__":
     
